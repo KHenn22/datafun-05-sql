0ad10fca9ea4dddb9d02d9c1d7d8ce504a2d68d7
Kevin Hennelly <ATCKH22@gmail.com>
2025-09-19 21:29:43 -1000

    update query code

--- ${workspaceFolder}/queries.sql
+++ ${workspaceFolder}/queries.sql
@@ -1,3 +1,18 @@
+-- Count rows in each table
 SELECT COUNT(*) AS airline_count FROM airlines;
 SELECT COUNT(*) AS crash_count FROM crashes;
-SELECT name FROM sqlite_master WHERE type='table';
+
+-- Show all airlines
+SELECT * FROM airlines;
+
+-- Show all crashes joined with airline names
+SELECT c.crash_id, c.flight_no, c.crash_date, c.location, c.fatalities, a.name AS airline_name
+FROM crashes c
+JOIN airlines a ON c.airline_id = a.airline_id;
+
+-- Top 3 deadliest crashes
+SELECT c.crash_id, c.flight_no, c.fatalities, a.name AS airline_name
+FROM crashes c
+JOIN airlines a ON c.airline_id = a.airline_id
+ORDER BY c.fatalities DESC
+LIMIT 3;
